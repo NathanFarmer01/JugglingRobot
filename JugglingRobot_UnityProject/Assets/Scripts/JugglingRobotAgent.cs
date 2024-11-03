daa6df9e86e7bf8ad1795f232175438181215f08
Nathan Farmer <n.farmer54583@gmail.com>
2024-11-03 16:21:02 -0500

    Created juggling robot agent base script

---
+++ JugglingRobot_UnityProject/Assets/Scripts/JugglingRobotAgent.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class JugglingRobotAgent : Agent
+{
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        
+    }
+}
